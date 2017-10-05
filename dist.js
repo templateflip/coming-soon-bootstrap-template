252583ebfb8789e355e06c4390bca48d8cde317d
Kanishk Kunal <kanishk.kunal@hotmail.com>
2017-10-05 13:05:17 +0530

    Creating Dist

--- dist.js
+++ dist.js
@@ -10,11 +10,44 @@ if (!which('7z')) {
 cd('public');
 
 var inPath = '**'// folder to zip
-var outPath = path.join(__dirname, package.name + '_' + package.version.split('.').join('-') + '.zip') // name of output zip file
+
+var licenseFree = path.join(__dirname, 'license-free.txt')
+
+var licensePremium = path.join(__dirname, 'license-premium.txt')
+var sourcePremium = path.join(__dirname, 'styles/')
+
+var outPathFree = path.join(__dirname, package.name + '_free_' + package.version.split('.').join('-') + '.zip') // name of output zip file
+var outPathPremium = path.join(__dirname, package.name + '_premium_' + package.version.split('.').join('-') + '.zip') // name of output zip file
+
+rm(outPathFree)
+
+rm(outPathPremium)
 
 
-// Run external tool synchronously
-if (exec('7z a '+ outPath + ' ' + inPath).code !== 0) {
+// Run external tool synchronously for free version
+if (exec('7z a "'+ outPathFree + '" "' + inPath +'"').code !== 0) {
+  echo('Error: 7z commit failed');
+  exit(1);
+}
+
+if (exec('7z a "'+ outPathFree + '" "' + licenseFree +'"').code !== 0) {
+  echo('Error: 7z commit failed');
+  exit(1);
+}
+
+// Run external tool synchronously for premium version
+if (exec('7z a "'+ outPathPremium + '" "' + inPath +'"').code !== 0) {
+  echo('Error: 7z commit failed');
+  exit(1);
+}
+
+
+if (exec('7z a "'+ outPathPremium + '" "' + sourcePremium +'"').code !== 0) {
+  echo('Error: 7z commit failed');
+  exit(1);
+}
+
+if (exec('7z a "'+ outPathPremium + '" "' + licensePremium +'"').code !== 0) {
   echo('Error: 7z commit failed');
   exit(1);
 }
